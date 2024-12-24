36f9952345e9d2dd08ef87c6b21a4c817f244da4
Cauã Rinaldi <cauarinaldi451@gmail.com>
2024-12-24 12:55:33 -0300

    Update context.ts

--- pages/api/context.ts
+++ pages/api/context.ts
@@ -8,5 +8,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
+ // @ts-ignore
   res.status(200).json({ hasContext: !!globalThis[Symbol.for('@vercel/request-context')]?.get() })
 }
